01b5a1764cf3c82d2d9ab8df631eca7d8a3d5551
Kyomu-Rin <rinawkmbi4k8@gmail.com>
2021-11-04 22:45:44 +0900

    create drawerPage

--- lib/screen/study_rooms/rooms_top_page.dart
+++ lib/screen/study_rooms/rooms_top_page.dart
@@ -1,8 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:mokumoku_a/menu/questionnaire/question_first.dart';
-import 'package:mokumoku_a/menu/setting.dart';
+import 'package:mokumoku_a/menu/drawer.dart';
 import 'package:mokumoku_a/screen/study_rooms/add_study_room.dart';
 import 'package:mokumoku_a/screen/study_rooms/study_page.dart';
 import 'package:mokumoku_a/utils/firebase.dart';
@@ -108,84 +107,7 @@ class _RoomsTopPageState extends State<RoomsTopPage> {
               centerTitle: true,
             ),
 
-            drawer: Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-
-                  // ここにMokuMokuのメインキャラかロゴを入れる
-                  DrawerHeader(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      image: DecorationImage(
-                        image: AssetImage('images/MokuMoku_logo_01.png'),
-                      ),
-                    ),
-                    child: null,
-                  ),
-
-                  ListTile(
-                    leading: Icon(Icons.info_outline),
-                    title: Text(
-                      'MokuMokuについて',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                    onTap: (){
-                      print('MokuMokuについての説明');
-                    },
-                  ),
-
-                  ListTile(
-                    leading: Icon(Icons.feed_outlined),
-                    title: Text(
-                      'アンケート',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                    onTap: (){
-                      // 一旦戻れるように変更
-                      Navigator.pop(context);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => QuestionFirstPage(),
-                        ),
-                      );
-                    },
-                  ),
-
-                  ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text(
-                      '設定',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => SettingPage(widget.uid),
-                      ));
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.share),
-                    title: Text(
-                      'シェア',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                    onTap: (){
-                      print('SHARE');
-                    },
-                  ),
-                ],
-              ),
-            ),
+            drawer: DrawerPage(widget.uid),
 
             body: TabBarView(
               children: [
