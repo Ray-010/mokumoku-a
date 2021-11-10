e7d1504b66250e5133d00be31a17e221225e78fb
Kyomu-Rin <rinawkmbi4k8@gmail.com>
2021-11-10 20:58:21 +0900

    url

--- lib/menu/drawer.dart
+++ lib/menu/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mokumoku_a/menu/questionnaire/question_first.dart';
 import 'package:mokumoku_a/menu/setting.dart';
+import 'package:mokumoku_a/menu/test.dart';
 
 class DrawerPage extends StatelessWidget {
   String uid;
@@ -51,7 +52,7 @@ class DrawerPage extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => QuestionFirstPage(),
+                  builder: (context) => MyHomePage(),
                 ),
               );
             },
